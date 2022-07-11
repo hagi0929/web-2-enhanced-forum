fdd513315c280c70209c6c1815371b4d96c36656
pc <ministove3yo@gmail.com>
2022-07-11 15:45:15 +0900

    .

--- editFinal.php
+++ editFinal.php
@@ -1,16 +1,16 @@
 <?php
 include_once('init.php');
-$rcvPassword = $_POST["Password"];
-$rcvID = $_POST["ID"];
-$rcvTitle = $_POST["Title"];
-$rcvContent = $_POST["Content"];
-
-$article = mysqli_fetch_array(mysqli_query(connect(), "SELECT * FROM articledb where id = " . $rcvID));
-
-$authorID = $article['authorId'];
-$authorPassword = mysqli_fetch_array(mysqli_query(connect(), "SELECT password FROM users where userId = " . $authorID))['password'];
-if ($rcvPassword == $authorPassword) {
-    mysqli_query(connect(), "UPDATE articledb SET title = '" . $rcvTitle . "', content = '" . $rcvContent . "', uploadDate = now() WHERE id = " . $rcvID);
+$filtered = array(
+    "Password" => mysqli_real_escape_string(connect(), $_POST["Password"]),
+    "ID" => mysqli_real_escape_string(connect(), $_POST["ID"]),
+    "Title" => mysqli_real_escape_string(connect(), $_POST["Title"]),
+    "Content" => mysqli_real_escape_string(connect(), $_POST["Content"])
+);
+$article = mysqli_fetch_array(mysqli_query(connect(), "SELECT * FROM articledb where id ={$filtered["ID"]}"));
+$authorID = mysqli_real_escape_string(connect(), $article['authorId']);
+$authorPassword = mysqli_fetch_array(mysqli_query(connect(), "SELECT password FROM users where userId = {$authorID}")['password']);
+if ($article['Password'] == $authorPassword) {
+    mysqli_query(connect(), "UPDATE articledb SET title = '" . $filtered["Title"] . "', content = '" . $filtered["Content"] . "', uploadDate = now() WHERE id = " . $filtered["ID"]);
 } else {
     echo '<script>alert("access denied")</script>';
 }
--- view.php
+++ view.php
@@ -1,39 +1,37 @@
 <?php
 include_once('init.php');
-$articleId = $_GET['id'];
+top('view profile');
+headerInit();
+if (isset($_GET['id'])) {
+    $articleId = mysqli_real_escape_string(connect(), $_GET['id']);
+} else {
+    header("Location:index.php");
+}
 $filterArticle = "SELECT id, title, displayName, uploadDate, userId, displayName, content FROM articledb LEFT JOIN users ON articledb.authorId = users.userId WHERE articledb.id =" . $articleId;
 $query = mysqli_query(connect(), $filterArticle);
-$row = mysqli_fetch_array($query);
-?>
-
-<?php top('view profile');
-headerInit(); ?>
+$row = mysqli_fetch_array($query); ?>
     <script src="main.js"></script>
     <div class="container_contents">
         <div class="wrapper">
             <div class="nav">
                 <form action="deleteFinal.php" method="post" id="deleteSubmit">
-                    <input type="hidden" name="ID" value=<?php echo $articleId ?>>
+                    <input type="hidden" name="ID" value=<?= $articleId ?>>
                 </form>
                 <form action="edit.php" method="post" id="editSubmit">
-                    <input type="hidden" name="ID" value=<?php echo $articleId ?>>
+                    <input type="hidden" name="ID" value=<?= $articleId ?>>
                 </form>
                 <a class="btn" href="javascript: passwordNSubmit('deleteSubmit')">Delete</a>
                 <a class="btn" href="javascript: passwordNSubmit('editSubmit')">Edit</a>
             </div>
-            <?php
-            echo '
-        <div class="view">
-            <div class="view-title">
-                <h3>' . $row['title'] . '</h3>
-                <span>' . $row['displayName'] . ' | ' . $row['uploadDate'] . '</span>
+            <div class="view">
+                <div class="view-title">
+                    <h3><?= $row['title'] ?></h3>
+                    <span><?= $row['displayName'] ?> | <?= $row['uploadDate'] ?></span>
+                </div>
+                <div class="view-content">
+                    <span><?= $row['content'] ?></span>
+                </div>
             </div>
-            <div class="view-content">
-                <span>' . $row['content'] . '</span>
-            </div>
-        </div>';
-
-            ?>
         </div>
     </div>
 <?php footerInit(); ?>
